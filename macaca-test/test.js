ed1059d17f91cc934ba70c3dbb6fb96f05a010ff
xudafeng <xudafeng@126.com>
2015-12-12 15:31:15 +0800

    support custom dir

---
+++ macaca-test/test.js
@@ -0,0 +1,20 @@
+/* ================================================================
+ * macaca-mocha by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Aug 26 2015 11:55:14 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+describe('macaca-test', function() {
+  it('should be ok', function() {
+    'macaca'.should.be.ok;
+  });
+});
